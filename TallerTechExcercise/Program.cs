facf80e9dcc8ef4542029497463f00cf549ea6b8
Daniel Silva Jimenez <djimenez@seamgroup.com>
2022-04-05 10:08:57 -0300

    first commit

---
+++ TallerTechExcercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TallerTechExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
